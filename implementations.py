6fb73843cdf5ce8c2b8d6eb67731108f40cdd216
Leilabenj <leila.benjelloun@epfl.ch>
2025-09-22 16:43:51 +0200

    first organization of repo

---
+++ implementations.py
@@ -0,0 +1,13 @@
+#Here we have to put all the implementations of the algorithms
+
+def logistic_regression(x_train, y_train, x_test, y_test):
+    """
+    This function implements the logistic regression algorithm.
+    """
+    pass
+
+def decision_tree(x_train, y_train, x_test, y_test):
+    """
+    This function implements the decision tree algorithm.
+    """
+    pass
